7c6c2e57233b61dee70a3656797adca6c1f874e5
Alexander Corley <xandy@corley.org>
2017-03-22 11:51:56 -0700

    Add tests for the dropPiece function

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -14,7 +14,30 @@ class PiezasTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
-TEST(PiezasTest, sanityCheck)
-{
-	ASSERT_TRUE(true);
+TEST(PiezasTest, dropPieceLowerEdge) {
+	Piezas pie;
+  ASSERT_EQ((char)pie.dropPiece(0), 'X');
+}
+TEST(PiezasTest, dropPieceUpperEdge) {
+  Piezas pie;
+  ASSERT_EQ((char)pie.dropPiece(3), 'X');
+}
+TEST(PiezasTest, dropPieceMid) {
+  Piezas pie;
+  ASSERT_EQ((char)pie.dropPiece(2), 'X');
+}
+TEST(PiezasTest, dropPieceOutOfBoundsBelow) {
+  Piezas pie;
+  ASSERT_EQ((char)pie.dropPiece(-1), '?');
+}
+TEST(PiezasTest, dropPieceOutOfBoundsAbove) {
+  Piezas pie;
+  ASSERT_EQ((char)pie.dropPiece(5), '?');
+}
+TEST(PiezasTest, dropPieceColumnFull) {
+  Piezas pie;
+  pie.dropPiece(0);
+  pie.dropPiece(0);
+  pie.dropPiece(0);
+  ASSERT_EQ((char)pie.dropPiece(0), ' ');
 }
